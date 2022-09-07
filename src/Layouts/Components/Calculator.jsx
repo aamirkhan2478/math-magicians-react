7282b933d088b0845b204655dfecc500b8aaa213
Aamir Khan <aamir.khan2478@gmail.com>
2022-09-08 03:57:45 +0500

    Refectored class-base component to function-base component with hooks.

--- src/Layouts/Components/Calculator.jsx
+++ src/Layouts/Components/Calculator.jsx
@@ -1,92 +1,87 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import calculate from "../../logic/calculate";
 import "../Assets/styles/calculator.css";
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+  const { total, next, operation } = state;
 
-  updateCalc = (e) => {
-    this.setState((state) => calculate(state, e.target.textContent));
+  const updateCalc = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
   };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="container">
-        <div className="text-box">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="grid-container">
-          <button className="btn" value="AC" onClick={this.updateCalc}>
-            AC
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            +/-
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            %
-          </button>
-          <button className="btn btn-orange" onClick={this.updateCalc}>
-            +
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            7
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            8
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            9
-          </button>
-          <button className="btn btn-orange" onClick={this.updateCalc}>
-            x
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            4
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            5
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            6
-          </button>
-          <button className="btn btn-orange" onClick={this.updateCalc}>
-            &divide;
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            1
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            2
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            3
-          </button>
-          <button className="btn btn-orange" onClick={this.updateCalc}>
-            -
-          </button>
-          <button className="btn btn-block" onClick={this.updateCalc}>
-            0
-          </button>
-          <button className="btn" onClick={this.updateCalc}>
-            .
-          </button>
-          <button className="btn btn-orange" onClick={this.updateCalc}>
-            =
-          </button>
-        </div>
+  return (
+    <div className="container">
+      <div className="text-box">
+        {total}
+        {operation}
+        {next}
       </div>
-    );
-  }
-}
+      <div className="grid-container">
+        <button className="btn" value="AC" onClick={updateCalc}>
+          AC
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          +/-
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          %
+        </button>
+        <button className="btn btn-orange" onClick={updateCalc}>
+          +
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          7
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          8
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          9
+        </button>
+        <button className="btn btn-orange" onClick={updateCalc}>
+          x
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          4
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          5
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          6
+        </button>
+        <button className="btn btn-orange" onClick={updateCalc}>
+          &divide;
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          1
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          2
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          3
+        </button>
+        <button className="btn btn-orange" onClick={updateCalc}>
+          -
+        </button>
+        <button className="btn btn-block" onClick={updateCalc}>
+          0
+        </button>
+        <button className="btn" onClick={updateCalc}>
+          .
+        </button>
+        <button className="btn btn-orange" onClick={updateCalc}>
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
