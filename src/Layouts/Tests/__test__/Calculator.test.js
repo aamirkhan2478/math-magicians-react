e333da91825b797936c5737ff3890f2fbce5d2dc
Moathal <moathal.s.kachi@gmail.com>
2022-09-15 13:21:56 +0300

    Add two tests for calculate.js
    Add test for operate.js
    
    Test navbar component rendering
    
    Test Calculator component rendering

--- src/Layouts/Tests/__test__/Calculator.test.js
+++ src/Layouts/Tests/__test__/Calculator.test.js
@@ -1,7 +1,7 @@
-import Calculator from "../../Components/Calculator";
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
+import Calculator from '../../Components/Calculator';
 
-it("renders calculator correctly", () => {
+it('renders calculator correctly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
 });
--- src/Layouts/Tests/__test__/Navbar.test.js
+++ src/Layouts/Tests/__test__/Navbar.test.js
@@ -1,7 +1,7 @@
-import Navbar from "../__mock__/Navbar";
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
+import Navbar from '../__mock__/Navbar';
 
-it("renders navbar correctly", () => {
+it('renders navbar correctly', () => {
   const tree = renderer.create(<Navbar />).toJSON();
   expect(tree).toMatchSnapshot();
 });
